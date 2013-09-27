d11d5ec5c4fc0011cf7d60d0bbca2517362e0a95
kosuke <kosuke.n27@gmail.com>
2013-09-28 02:48:47 +0900

    fact chunk

--- wav_file_handle.c
+++ wav_file_handle.c
@@ -117,16 +117,32 @@ int read_data_chunk(FILE *fp, DataChunk *chunk) {
 
     DataChunk temp;
     fread(&temp.chunk_type, sizeof(temp.chunk_type), 1, fp);
-    if (memcmp(temp.chunk_type, "data", 4)) {
-        fseek(fp, - sizeof(temp.chunk_type), SEEK_CUR);
 
-        unsigned short size_of_ext = 0;
-        fread(&size_of_ext, sizeof(size_of_ext), 1, fp);
-        fseek(fp, size_of_ext, SEEK_CUR);
-        fread(&temp.chunk_type, sizeof(temp.chunk_type), 1, fp);
+    unsigned char seeked_flg = 0;
+    while (1) {
         if (memcmp(temp.chunk_type, "data", 4)) {
-            fprintf(stderr, "[ERROR] This file has not 'data' chunk.\n");
-            return EXIT_FAILURE;
+            if (memcmp(temp.chunk_type, "fact", 4) == 0) {
+                fseek(fp, 12, SEEK_CUR);
+                fread(&temp.chunk_type, sizeof(temp.chunk_type), 1, fp);
+            }
+            else if (! seeked_flg) {
+                fseek(fp, - sizeof(temp.chunk_type), SEEK_CUR);
+
+                // extended fmt chunk
+                unsigned short size_of_ext = 0;
+                fread(&size_of_ext, sizeof(size_of_ext), 1, fp);
+                fseek(fp, size_of_ext, SEEK_CUR);
+
+                fread(&temp.chunk_type, sizeof(temp.chunk_type), 1, fp);
+            }
+            else {
+                fprintf(stderr, "[ERROR] This file has not 'data' chunk.\n");
+                return EXIT_FAILURE;
+            }
+            seeked_flg++;
+        }
+        else {
+            break;
         }
     }
 
